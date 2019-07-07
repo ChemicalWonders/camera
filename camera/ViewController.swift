519a19a914552153f50c4408bc6dda4297907992
Kevin Chan <kchan039@gmail.com>
2019-07-06 18:20:26 -0700

    Initial Commit

---
+++ camera/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  camera
+//
+//  Created by Kevin Chan on 7/6/19.
+//  Copyright © 2019 Kevin Chan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
